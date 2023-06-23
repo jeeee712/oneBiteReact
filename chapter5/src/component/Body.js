3b4e08526e4486597f28361e1bbc7e17127053d8
jeeee712 <ohjiyi@naver.com>
2023-06-23 18:50:38 +0900

    23.06.23

--- chapter5/src/component/Body.js
+++ chapter5/src/component/Body.js
@@ -1,6 +1,29 @@
-function Body({ children }) {
-  console.log(children);
-  return <div className="body">{children}</div>;
+import { useState } from "react";
+
+function Viewer() {
+  console.log("viewer component update!");
+  return <div>Viewer</div>;
+}
+
+function Body() {
+  const [number, setNumber] = useState(0);
+  const onIncrease = () => {
+    setNumber(number + 1);
+  };
+  const onDecrease = () => {
+    setNumber(number - 1);
+  };
+
+  return (
+    <div>
+      <h2>{number}</h2>
+      <Viewer />
+      <div>
+        <button onClick={onDecrease}>-</button>
+        <button onClick={onIncrease}>+</button>
+      </div>
+    </div>
+  );
 }
 
 export default Body;
